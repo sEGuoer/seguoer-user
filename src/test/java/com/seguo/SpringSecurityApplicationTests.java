4f13540d09d930da5dbcb6643c687c46b00f447c
sEGuoer <709074535@qq.com>
2023-10-30 17:06:15 +0800

    Add application.yml

--- src/test/java/com/seguo/SpringSecurityApplicationTests.java
+++ src/test/java/com/seguo/SpringSecurityApplicationTests.java
@@ -1,13 +1,18 @@
 package com.seguo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
+    @Value("${spring.application.name}")
+    private String msg;
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals("lms",msg);
     }
 
 }
