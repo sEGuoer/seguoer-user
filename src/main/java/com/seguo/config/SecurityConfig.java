238e26c0cb697030ab0cbc31a6602d59236e6991
sEGuoer <709074535@qq.com>
2023-10-31 16:50:05 +0800

    SecurityConfig

---
+++ src/main/java/com/seguo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.seguo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .loginProcessingUrl("login")
+                        .failureForwardUrl("/")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                )
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers("/","/login","/build/**","/vendor/**").permitAll()
+                        .requestMatchers("/backend/**").hasAnyAuthority("admin")
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
