355406a43c799489bc75a503a688f9556e3cd780
sEGuoer <709074535@qq.com>
2023-10-31 16:49:27 +0800

    change name

--- src/main/java/com/seguo/controller/HomepageController.java
+++ src/main/java/com/seguo/controller/HomepageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class homepageController {
+public class HomepageController {
     @GetMapping("index")
     String index() {
         return "index";
