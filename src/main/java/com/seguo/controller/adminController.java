421a04ed148c689c9cdb75a45fef3ef011571c1f
sEGuoer <709074535@qq.com>
2023-10-31 14:17:35 +0800

    admin page content by thymeleaf and controller

---
+++ src/main/java/com/seguo/controller/adminController.java
@@ -0,0 +1,18 @@
+package com.seguo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+public class adminController {
+    @GetMapping("dashboard")
+    String index() {
+        return "backend/dashboard";
+    }
+    @GetMapping("users")
+    String login() {
+        return "backend/users";
+    }
+}
