2632fac1f92372c077de9800a9d7da3712d83f14
sEGuoer <709074535@qq.com>
2023-10-31 14:59:38 +0800

    file move

--- src/main/java/com/seguo/properties/AdminUseProperties.java
+++ src/main/java/com/seguo/properties/AdminUseProperties.java
@@ -1,14 +1,19 @@
 package com.seguo.properties;
 
 import com.seguo.po.AdminPageContent;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
-
 
 @ConfigurationProperties(prefix = "my.admin")
 @Data
 public class AdminUseProperties {
  private Map<String, AdminPageContent> adminPageContentMap;
+ public String getAdminContent(String name){
+  return adminPageContentMap.get(name).getName();
+ }
 }
+
