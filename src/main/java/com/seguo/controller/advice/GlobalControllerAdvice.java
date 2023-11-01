23f0513bb43d1faa808757017dfd42c1844275fe
sEGuoer <709074535@qq.com>
2023-11-01 10:39:33 +0800

    Dynamic add active class
    ControllerAdvice + ModelAttribute

---
+++ src/main/java/com/seguo/controller/advice/GlobalControllerAdvice.java
@@ -0,0 +1,13 @@
+package com.seguo.controller.advice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    @ModelAttribute("requestURI")
+    String getRequestServletPath(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+}
