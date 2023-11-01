e40db980f10611bc9d32660eb8fc0bb30743d184
sEGuoer <709074535@qq.com>
2023-11-01 16:20:31 +0800

    set adminApp logout menu and /login page test

--- src/test/java/com/seguo/SpringSecurityApplicationTests.java
+++ src/test/java/com/seguo/SpringSecurityApplicationTests.java
@@ -1,18 +1,32 @@
 package com.seguo;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringSecurityApplicationTests {
     @Value("${spring.application.name}")
     private String msg;
-
+    @Autowired
+    MockMvc mvc;
     @Test
     void contextLoads() {
         Assertions.assertEquals("lms",msg);
     }
-
+    @Test
+    void login() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(MockMvcResultMatchers.view().name("login"))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("登录")))
+        ;
+    }
 }
