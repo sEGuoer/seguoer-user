08308fca0576fcdcc52f484f2f55d4715b90970b
sEGuoer <709074535@qq.com>
2023-11-03 14:34:49 +0800

    change to TUnified format

--- src/test/java/com/seguo/WithMockUserBaseTest.java
+++ src/test/java/com/seguo/WithMockUserBaseTest.java
@@ -1,6 +1,6 @@
 package com.seguo;
 import org.springframework.security.test.context.support.WithMockUser;
 
-@WithMockUser(username = "admin",password = "password" ,roles = {"admin"})
+@WithMockUser(username = "admin", password = "password", roles = {"admin"})
 public class WithMockUserBaseTest {
 }
