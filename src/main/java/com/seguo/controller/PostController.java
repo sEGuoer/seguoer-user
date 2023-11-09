728817a7b7de58689c9007d5ad78aaeef38a3ac0
sEGuoer <709074535@qq.com>
2023-11-09 14:23:03 +0800

    backend/posts
    - add migration
    - update menus configuration
    - entity Post
    - repository PostRepository
    - service PostService + PostServiceImpl
    - controller PostController
    - template backend/blog/index

---
+++ src/main/java/com/seguo/controller/PostController.java
@@ -0,0 +1,32 @@
+package com.seguo.controller;
+
+
+import com.seguo.entity.Post;
+import com.seguo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/admin")
+public class PostController {
+    @Autowired
+    PostService postService;
+
+    @GetMapping("blogs")
+    String index(Model model,
+                 @RequestParam("page") Optional<Integer> page,
+                 @RequestParam("size") Optional<Integer> size) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+        Page<Post> pageContent = postService.findAll(currentPage, pageSize);
+        model.addAttribute("page", pageContent);
+        return "backend/blog/index";
+    }
+}
