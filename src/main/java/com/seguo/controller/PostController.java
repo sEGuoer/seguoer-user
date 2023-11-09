3073534111920ad8afd12d73e5af19f555452378
sEGuoer <709074535@qq.com>
2023-11-09 14:56:31 +0800

    Create New Post page

--- src/main/java/com/seguo/controller/PostController.java
+++ src/main/java/com/seguo/controller/PostController.java
@@ -3,13 +3,13 @@ package com.seguo.controller;
 
 import com.seguo.entity.Post;
 import com.seguo.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -28,5 +28,11 @@ public class PostController {
         Page<Post> pageContent = postService.findAll(currentPage, pageSize);
         model.addAttribute("page", pageContent);
         return "backend/blog/index";
+    }
+
+    @GetMapping("blog/create")
+    String create(Model model) {
+        model.addAttribute("post", new Post());
+        return "backend/blog/create";
     }
 }
--- src/test/java/com/seguo/controller/PostControllerTest.java
+++ src/test/java/com/seguo/controller/PostControllerTest.java
@@ -4,9 +4,10 @@ package com.seguo.controller;
 import com.seguo.WithMockUserForAdminBaseTest;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+@WithUserDetails(userDetailsServiceBeanName = "jpaUserDetailsService", value = "admin@example.com")
 class PostControllerTest extends WithMockUserForAdminBaseTest {
 
     @Test
@@ -14,6 +15,13 @@ class PostControllerTest extends WithMockUserForAdminBaseTest {
         mvc.perform(MockMvcRequestBuilders.get("/admin/blogs"))
                 .andExpect(MockMvcResultMatchers.view().name("backend/blog/index"))
                 .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Post 管理")))
+        ;
+    }
+    @Test
+    void create() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/admin/blog/create"))
+                .andExpect(MockMvcResultMatchers.view().name("backend/blog/create"))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Create New Post")))
         ;
     }
 }
