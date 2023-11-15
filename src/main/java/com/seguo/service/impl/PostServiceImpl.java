066fa14c834731e5fc314acbe99932852befacbb
sEGuoer <709074535@qq.com>
2023-11-15 15:57:41 +0800

    findAll PageRequest.of(**,**,sort)

--- src/main/java/com/seguo/service/impl/PostServiceImpl.java
+++ src/main/java/com/seguo/service/impl/PostServiceImpl.java
@@ -23,7 +23,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public Page<Post> findAll(int pageNumber, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize,Sort.by("id").descending());
         return this.postRepository.findAll(pageable);
     }
     @Override
